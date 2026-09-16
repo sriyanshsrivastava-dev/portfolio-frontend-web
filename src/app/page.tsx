3e0f1893fe89d58262d76e5ec55d911db11dcf76
Sriyansh Srivastava <sriyanshsrivastava.dev@gmail.com>
2026-09-17 03:33:02 +0530

    chore: add project structure

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+      <>
+        <div className="flex flex-col h-screen w-full justify-center items-center">
+          <h1 className="text-3xl">Welcome to portfolio website.</h1>
+          <p>Website is currently being developed</p>
+        </div>
+      </>
+  );
+}
