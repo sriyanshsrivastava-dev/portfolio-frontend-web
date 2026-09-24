fac0e0e42dfe7db033b0c43580a1600a4c920211
Sriyansh Srivastava <sriyanshsrivastava.dev@gmail.com>
2026-09-24 08:59:21 +0530

    feat: add hero section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import HeroSection from "@/components/Main/HeroSection";
+
 export default function Home() {
   return (
-      <>
+      <main>
+          <HeroSection/>
         <div className="flex flex-col h-screen w-full justify-center items-center">
           <h1 className="text-3xl">Welcome to portfolio website.</h1>
-          <p>Website is currently being developed</p>
+          <p>Website is currently being developed.</p>
         </div>
-      </>
+      </main>
   );
 }
